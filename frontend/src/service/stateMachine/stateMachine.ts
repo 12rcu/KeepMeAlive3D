e982e432d779a703fbda960eabf3c38318987bed
matthias <matthias.klenz@outlook.com>
2025-11-02 15:23:23 +0100

    fix duplicate type

--- frontend/src/service/stateMachine/stateMachine.ts
+++ frontend/src/service/stateMachine/stateMachine.ts
@@ -1,12 +1,6 @@
 import service from "@/service/service.ts";
 import type { AxiosResponse } from "axios";
-
-export interface StateData {
-  id: string
-  posX: number
-  poxY: number
-  connectedTo: string[]
-}
+import type { StateData } from "@/scene/stateMachine/data.ts";
 
 export function getStateMachineForDT(dtId: string, participantId: string): Promise<AxiosResponse<StateData[]>> {
   return service.get<StateData[]>(`/api/dt/${dtId}/statemachine/${participantId}`);
